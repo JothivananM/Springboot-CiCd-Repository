c1f68185f2c17dc7ac3c98dd82af77b83bb9b1df
Jothivanan <jothivaanan.madheswaran@nipurnait.com>
2022-05-27 13:17:36 +0530

    Initialization

---
+++ src/main/java/com/springboot/cicd/springbootcicd/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.springboot.cicd.springbootcicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+    
+    @GetMapping
+    public String demo(){
+        return "LoL";
+    }
+}
